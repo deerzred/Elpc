3c7b85258641e7ecdd78ecd4d8b290b83bc31cd8
yosefgizachew <yeyeadmss@gmail.com>
2025-10-30 07:52:05 +0300

    Fixed forms

--- Elpc/elpcc/forms.py
+++ Elpc/elpcc/forms.py
@@ -1,1 +1,6 @@
-from django
+from django import forms
+from django.contrib.auth.models import User
+
+class Signup(forms.Form):
+    username = forms.CharField(max_length=150, label='Username')
+    
